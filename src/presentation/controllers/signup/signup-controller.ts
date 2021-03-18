72055592add0c229f10d31511ae878e41e919cae
kaiquecaires <kaique.caires@yahoo.com>
2021-03-18 07:39:10 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -2,21 +2,12 @@ import { Controller, HttpRequest, HttpResponse, MissignParamError, badRequest } 
 
 export class SignupController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, name, password, passwordConfirmation } = httpRequest.body
-    if (!email) {
-      return badRequest(new MissignParamError('email'))
-    }
-
-    if (!name) {
-      return badRequest(new MissignParamError('name'))
-    }
-
-    if (!password) {
-      return badRequest(new MissignParamError('password'))
-    }
-
-    if (!passwordConfirmation) {
-      return badRequest(new MissignParamError('passwordConfirmation'))
+    const fields = httpRequest.body
+    const requiredFields = ['email', 'name', 'password', 'passwordConfirmation']
+    for (const field of requiredFields) {
+      if (!fields[field]) {
+        return badRequest(new MissignParamError(field))
+      }
     }
   }
 }
